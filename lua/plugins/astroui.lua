d1b2974012f448e65856cf09eff273e7b175402c
timakaa <timur.djeletyan.2009@gmail.com>
2025-10-28 00:08:29 +0200

    feat: background for neo tree

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -15,10 +13,15 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+        -- Neo-tree hover highlight
+        NeoTreeCursorLine = { bg = "#2a2a2a" }, -- Dark gray background for hovered items
+        -- Alternative: you can also use these highlight groups
+        -- NeoTreeFileNameOpened = { bg = "#2a2a2a" },
+        -- NeoTreeDirectoryName = { bg = "#2a2a2a" },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
+        -- You can customize colors specifically for astrodark theme here
+        NeoTreeCursorLine = { bg = "#3a3a3a" }, -- Slightly lighter for astrodark
       },
     },
     -- Icons can be configured throughout the interface
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,5 +8,18 @@ return {
         hide_dotfiles = false -- Do not hide dotfiles
       },
     },
+    default_component_configs = {
+      name = {
+        use_git_status_colors = true,
+        highlight_opened_files = true, -- Enable highlighting for opened files
+      },
+    },
+    window = {
+      mappings = {
+        -- Enable cursor line in neo-tree
+        ["<CR>"] = "open",
+        ["o"] = "open",
+      },
+    },
   },
 }
