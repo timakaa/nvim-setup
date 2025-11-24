5f1cecf0f0566f29f3fefdb1b07cdae8f97bfd87
luffy977338 <timur.djeletyan.2009@gmail.com>
2025-11-24 13:55:37 +0300

    fix

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-vim.env.GEMINI_API_KEY="AIzaSyChEmgepKox_6YI0Fq9gtPefx6cCetaVck"
+vim.env.GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
